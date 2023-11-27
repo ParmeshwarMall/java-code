b1d5c204111dd41c9c4c8fb4a0e16ed80b638153
ParmeshwarMall <142672221+ParmeshwarMall@users.noreply.github.com>
2023-11-27 22:00:22 +0530

    Add files via upload

---
+++ Backtracking/RatInADeadMazeFourDirn.java
@@ -0,0 +1,38 @@
+package Backtracking;
+
+public class RatInADeadMazeFourDirn {
+    public static void main(String[] args) {
+        int rows=3;
+        int cols=3;
+        boolean [][] isVisited=new boolean[rows][cols];
+        int [][] maze={{1,0,1},
+                       {1,1,1},
+                       {0,1,1}};
+        RatGrid(0,0,rows-1,cols-1,"",isVisited);
+    }
+
+    static void RatGrid(int sr,int sc,int er,int ec,String s,boolean [][]isVisited)
+    {
+        if(sr<0 || sc<0)
+            return;
+        if(sr>er || sc>ec)
+            return;
+        if(isVisited[sr][sc]==true)
+            return;
+        if(sr==er && sc==ec)
+        {
+            System.out.println(s);
+            return;
+        }
+        isVisited[sr][sc]=true;
+        // go right
+        RatGrid(sr,sc+1,er,ec,s+"R",isVisited);
+        // go down
+        RatGrid(sr+1,sc,er,ec,s+"D",isVisited);
+        // go left
+        RatGrid(sr-1,sc,er,ec,s+"L",isVisited);
+        // go up
+        RatGrid(sr,sc-1,er,ec,s+"U",isVisited);
+        isVisited[sr][sc]=false;
+    }
+}
